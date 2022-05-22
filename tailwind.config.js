cba24773c975d4fe78dc4dd8d44da38985b6c25c
Yasaman Moeinian <yasimopv18@gmail.com>
2022-05-22 22:02:29 +0430

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: "jit",
+    content: ["./*.html"],
+    darkMode: "class",   //media or class
+    theme: {
+        fontFamily: {
+            vazir: ['Vazir']
+        },
+        extend: {},
+    },
+    plugins: [],
+}
